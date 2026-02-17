84d3dff0ba98bcc9d0d3dfd403481186bf071872
cherutkelman <cherut13@gmail.com>
2026-02-17 20:35:03 +0200

    feat: updater dialogs + bump to 1.1.6

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 
-const { app, BrowserWindow } = require('electron');
+
+const { app, BrowserWindow, dialog } = require("electron");
+const { autoUpdater } = require("electron-updater");
 const path = require('path');
 
 function createWindow() {
@@ -28,16 +30,60 @@ function createWindow() {
   // Remove the default menu bar for a cleaner look
   win.setMenuBarVisibility(false);
 }
-
 app.whenReady().then(() => {
   createWindow();
+autoUpdater.autoDownload = false;
+
+autoUpdater.on("checking-for-update", () => {
+  console.log("[updater] checking-for-update");
+});
+
+autoUpdater.on("update-available", () => {
+  console.log("[updater] update-available");
+  dialog.showMessageBox({
+    type: "info",
+    title: "עדכון זמין",
+    message: "נמצאה גרסה חדשה. להוריד עכשיו?",
+    buttons: ["כן", "לא"],
+    defaultId: 0
+  }).then((r) => {
+    if (r.response === 0) autoUpdater.downloadUpdate();
+  });
+});
+
+autoUpdater.on("update-not-available", () => {
+  console.log("[updater] update-not-available");
+  dialog.showMessageBox({
+    type: "info",
+    title: "אין עדכון",
+    message: "אין כרגע עדכון זמין."
+  });
+});
+
+autoUpdater.on("error", (err) => {
+  console.error("[updater] error", err);
+  dialog.showMessageBox({
+    type: "error",
+    title: "שגיאת עדכון",
+    message: String(err)
+  });
+});
+
+autoUpdater.on("update-downloaded", () => {
+  console.log("[updater] update-downloaded");
+  dialog.showMessageBox({
+    type: "info",
+    title: "העדכון ירד",
+    message: "העדכון הורד. התוכנה תיסגר ותעדכן.",
+    buttons: ["אישור"]
+  }).then(() => {
+    autoUpdater.quitAndInstall();
+  });
+});
+
+  autoUpdater.checkForUpdates(); // בדיקת עדכון בהפעלה
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 });
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
-});
-console.log("[build test] 1.1.5");
